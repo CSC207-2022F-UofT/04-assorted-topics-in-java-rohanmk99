ab2362685d55ea1fdc3ae23f5893b4b4546485fd
rohanmk99 <73184733+rohanmk99@users.noreply.github.com>
2022-10-08 00:11:18 -0400

    Changes updated

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,9 +11,9 @@
  */
 
 import java.util.List;
-public class DrivableTrader extends Trader<DrivableTrader>{
+public class DrivableTrader extends Trader{
 
-    public DrivableTrader(List<DrivableTrader> inventory, List<DrivableTrader> wishlist, int money) {
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
         super(inventory, wishlist, money);
         //TODO Auto-generated constructor stub
     }
@@ -22,16 +22,18 @@ public class DrivableTrader extends Trader<DrivableTrader>{
         super(money);
         //TODO Auto-generated constructor stub
     }
-        
-    @Override
-    public int getSellingPrice(DrivableTrader item){
+            
+
+    public int getSellingPrice(Drivable item){
         int super_price = super.getSellingPrice(item);
 
         if (super_price == Tradable.MISSING_PRICE) {
             return super_price;
         }
 
-        return super_price + ((Drivable) item).getMaxSpeed();
+        return super_price + item.getMaxSpeed();
     }
+
+   
 
 }
